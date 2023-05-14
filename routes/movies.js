e79861332e3eb29d61946ef1751b243e56df4272
ConnnorS <117893558+ConnnorS@users.noreply.github.com>
2023-05-14 22:06:41 +1000

    Commit

--- routes/movies.js
+++ routes/movies.js
@@ -1,4 +1,11 @@
-var express = require('express');
-var router = express.Router();
+// var express = require('express');
+// var router = express.Router();
 
-
+// router.get("/movies/search/", (req, res, next) => {
+//     req.db
+//         .from("movies.basics")
+//         .select("*")
+//         .then(rows => {
+//             res.json({Error: false, Message: "Success", data: rows});
+//         })
+// });
