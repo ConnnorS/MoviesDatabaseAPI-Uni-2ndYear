b4462d4f362744c72e638a14ce072ec45c802343
ConnnorS <117893558+ConnnorS@users.noreply.github.com>
2023-05-20 19:30:22 +1000

    Working on people/id

--- routes/people.js
+++ routes/people.js
@@ -2,14 +2,21 @@ var express = require('express');
 var router = express.Router();
 
 router.get("/:id", async (req, res, next) => {
+    // get the search params
     const id = req.params.id;
+
+    let result = {}
 
     // fetch the actor information
     try {
-        req.db
+        const mainActorInfo = await req.db
             .from("movies.names")
             .select("primaryName AS name", "birthYear", "deathYear")
-            .where("nconst", "=", id)
+            .where("nconst", "=", id);
+        
+        result = mainActorInfo;
+
+        // NOTE: get the actor's roles from here :NOTE //
     }
     // error handling 
     catch {
