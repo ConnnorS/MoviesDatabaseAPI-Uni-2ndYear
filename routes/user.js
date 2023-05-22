626ff40b2af561963849e448df0dcaed754ea794
ConnnorS <117893558+ConnnorS@users.noreply.github.com>
2023-05-22 13:11:49 +1000

    user/register working

--- routes/user.js
+++ routes/user.js
@@ -5,53 +5,47 @@ var router = express.Router();
 const bcrypt = require('bcrypt');
 
 // user/register
-router.post('/register', (req, res, next) => {
+router.post('/register', async (req, res, next) => {
     // get the email and password from the request
     const email = req.body.email;
     const password = req.body.password;
 
-    // try {
-    //     try {
-    //         if (!email || !password) {
-    //             return;
-    //         }
-    //     } catch {
-    //         res.status(400).json({
-    //             error: true,
-    //             message: "Request body incomplete - email and password needed."
-    //         });
-    //     }
-    // }
+    let status = 500;
+    let resJSON = { error: true, message: "Internal server error." };
 
-    // check whether the user has provided everything
-    if (!email || !password) {
-        res.status(400).json({
-            error: true,
-            message: "Request body incomplete - email and password needed."
-        });
-        return;
-    }
+    try {
+        // check whether the user has provided everything
+        if (!email || !password) {
+            status = 400;
+            resJSON = {
+                error: true,
+                message: "Request body incomplete - email and password needed."
+            };
 
-    // see if the user already exists
-    const query = req.db
-        .from("movies.users")
-        .select("*")
-        .where("email", "=", email);
-    query.then(users => {
-        if (users.length > 0) {
-            res.status(409).json({ success: false, message: "User already exists" });
-            return;
+            throw new Error;
         }
 
-        // if the user doesn't exist, add the user to our db
+        // determine if the user already exists
+        const users = await req.db
+            .from("movies.users")
+            .select("*")
+            .where("email", "=", email)
+
+        if (users.length > 0) {
+            status = 409;
+            resJSON = { error: true, message: "User already exists." };
+            throw new Error;
+        }
+
+        // add the user to the db
         const saltRounds = 10;
         const hash = bcrypt.hashSync(password, saltRounds);
+        res.status(201).json({ error: false, message: "User created." });
         return req.db.from("movies.users").insert({ email, hash });
-    })
-        .then(() => {
-            res.status(201).json({ error: false, message: "User created." });
-        })
-        .catch(e => res.status(500).json({ error: true, message: e.message }));
+    }
+    catch {
+        res.status(status).json(resJSON);
+    }
 });
 
 
