7d34f0e3aaa35ad22c02d348dc22a6de36639df3
ConnnorS <117893558+ConnnorS@users.noreply.github.com>
2023-05-22 13:48:32 +1000

    /user/login working

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+// environment variables
+require("dotenv").config();
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
--- routes/user.js
+++ routes/user.js
@@ -8,55 +8,84 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 // user/login
-router.get('/login', (req, res, next) => {
-    
-})
+router.post('/login', async (req, res, next) => {
+    const { email, password } = req.body;
 
+    // check if the request has everything
+    if (!email || !password) {
+        return res.status(400).json({
+            error: true,
+            message: "Request body incomplete, email and password needed."
+        });
+    }
 
+    // determine if the user is in the db
+    const users = await req.db
+        .from("movies.users")
+        .select("*")
+        .where("email", "=", email);
 
+    if (users.length === 0) {
+        return res.status(401).json({
+            error: true,
+            message: "The provided email does not exist."
+        });
+    }
+
+    // compare the passwords
+    const user = users[0];
+    const match = await bcrypt.compare(password, user.hash);
+
+    if (!match) {
+        return res.status(401).json({
+            error: true,
+            message: "Incorrect email or password."
+        });
+    }
+
+    // if the passwords are correct, return the bearer token
+    const expiresIn = 600;
+    const exp = Math.floor(Date.now() / 1000) + expiresIn;
+    const token = jwt.sign({ email, exp }, process.env.JWT_SECRET);
+
+    const refreshExpiresIn = 86400;
+    const refreshExp = Math.floor(Date.now() / 1000) + refreshExpiresIn;
+    const refreshToken = jwt.sign({email, refreshExp}, process.env.JWT_SECRET);
+
+    res.status(200).json({
+        bearerToken: { token, token_type: "Bearer", expiresIn },
+        refreshToken: {token: refreshToken, token_type: "Refresh", expiresIn: refreshExpiresIn}
+    });
+});
 
 // user/register
-router.post('/register', async (req, res, next) => {
-    // get the email and password from the request
-    const email = req.body.email;
-    const password = req.body.password;
+router.post('/register', async (req, res) => {
+    const { email, password } = req.body;
 
-    let status = 500;
-    let resJSON = { error: true, message: "Internal server error." };
-
-    try {
-        // check whether the user has provided everything
-        if (!email || !password) {
-            status = 400;
-            resJSON = {
-                error: true,
-                message: "Request body incomplete, email and password needed."
-            };
-
-            throw new Error;
-        }
-
-        // determine if the user already exists
-        const users = await req.db
-            .from("movies.users")
-            .select("*")
-            .where("email", "=", email)
-
-        if (users.length > 0) {
-            status = 409;
-            resJSON = { error: true, message: "User already exists." };
-            throw new Error;
-        }
-
-        // add the user to the db
-        const saltRounds = 10;
-        const hash = bcrypt.hashSync(password, saltRounds);
-        res.status(201).json({ error: false, message: "User created." });
-        return req.db.from("movies.users").insert({ email, hash });
+    // check if the request has everything
+    if (!email || !password) {
+        return res.status(400).json({
+            error: true,
+            message: "Request body incomplete, email and password needed."
+        });
     }
-    catch {
-        res.status(status).json(resJSON);
+
+    // check if the user already exists
+    const userExists = await req.db
+        .from("movies.users")
+        .select("*")
+        .where("email", "=", email);
+
+    if (userExists.length > 0) {
+        return res.status(409).json({ error: true, message: "User already exists." });
     }
+
+    // if all good, add the user to the db
+    const saltRounds = 10;
+    const hash = bcrypt.hashSync(password, saltRounds);
+    await req.db.from("movies.users").insert({ email, hash });
+
+    res.status(201).json({ error: false, message: "User created." });
 });
 
 
