fec19bc1312f59e86f4e94064579c3ddfff7c8fe
ConnnorS <117893558+ConnnorS@users.noreply.github.com>
2023-05-24 16:57:02 +1000

    Swagger in

--- app.js
+++ app.js
@@ -49,19 +49,24 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // routes
-app.use('/', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+app.get('/', (req, res) => {
+  // redirect to docs as to not take over all the routes
+  res.redirect('/docs'); 
+});
+
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 app.use('/people', peopleRouter);
 app.use('/movies', moviesRouter);
 app.use('/user', usersRouter);
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
